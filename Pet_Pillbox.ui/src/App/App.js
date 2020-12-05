7969752b38ed7f2458f1f6331ec4b30ece31f75d
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-12-05 13:33:50 -0600

    Google auth and user Post working

--- Pet_Pillbox.ui/src/App/App.js
+++ Pet_Pillbox.ui/src/App/App.js
@@ -2,11 +2,23 @@ import './App.scss';
 
 import Auth from '../components/shared/Auth/Auth'
 
+import fbConnection from "../helpers/data/connection";
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
+
+fbConnection();
+
+
 function App() {
   return (
     <div className="App">
       <h1>Pet Pillbox</h1>
       <Auth></Auth>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/Auth" component={Auth}></Route>
+          {/* <Route path="/Pets" component={Pets}></Route> */}
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- Pet_Pillbox.ui/src/components/shared/Auth/Auth.js
+++ Pet_Pillbox.ui/src/components/shared/Auth/Auth.js
@@ -1,10 +1,27 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 
+import authData from '../../../helpers/data/authData';
+
 class Auth extends React.Component {
+    // state = {
+    //     user: {
+    //       firebaseUid: '',
+    //     },
+    //   };
+    
+      loginClickEvent = (e) => {
+        e.preventDefault();
+        authData
+          .loginUser()
+          .catch(error => {
+            console.error('there was a problem logging in:', error);
+          });
+      };
+    
     render() {
         return (
-            <button type="button" className="btn btn-primary">Google Login</button>
+            <button onClick={this.loginClickEvent} type="button" className="btn btn-primary">Google Login</button>
         );
     }
 }
