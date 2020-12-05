36c667bed67384806fc052399ef058973d3e3e40
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-12-05 10:25:26 -0600

    wip

--- Pet_Pillbox.ui/src/App/App.js
+++ Pet_Pillbox.ui/src/App/App.js
@@ -1,9 +1,12 @@
 import './App.scss';
 
+import Auth from '../components/shared/Auth/Auth'
+
 function App() {
   return (
     <div className="App">
       <h1>Pet Pillbox</h1>
+      <Auth></Auth>
     </div>
   );
 }
---
+++ Pet_Pillbox.ui/src/components/shared/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+class Auth extends React.Component {
+    render() {
+        return (
+            <button type="button" className="btn btn-primary">Google Login</button>
+        );
+    }
+}
+
+export default Auth;
