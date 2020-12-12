7df6ef35fbf78994a310755bdec38b2e5c1c5a65
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-12-12 10:24:22 -0600

    fixed issue with re-posting existing user

--- Pet_Pillbox.ui/src/helpers/data/authData.js
+++ Pet_Pillbox.ui/src/helpers/data/authData.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 import axios from 'axios';
-import {baseUrl} from '../constants.json';
+import { baseUrl } from '../constants.json';
 
 // interceptors work by changing the outbound request before the xhr is sent 
 // or by changing the response before it's returned to our .then() method.
@@ -8,7 +8,7 @@ axios.interceptors.request.use(function (request) {
   const token = sessionStorage.getItem('token');
 
   if (token != null) {
-      request.headers.Authorization = `Bearer ${token}`;
+    request.headers.Authorization = `Bearer ${token}`;
   }
 
   return request;
@@ -19,57 +19,43 @@ axios.interceptors.request.use(function (request) {
 const checkIfNewUser = (uid) => {
   var existingUser = {};
   axios.get(`${baseUrl}/users/${uid}`)
-  .then((response) => {
-    existingUser.firebaseUid = response.data.firebaseUid;
-    console.log('response.data:', response.data);
-    console.log('return?:', existingUser)
-    return existingUser;
-  });
+    .then((response) => {
+      existingUser.firebaseUid = response.data.firebaseUid;
+      console.log('response.data:', response.data);
+      console.log('return?:', existingUser)
+      return existingUser;
+    });
   // console.log('exists:', existingUser.firebaseUid);
-  
+
 };
+
+const checkForExistingUser = (user) => new Promise((resolve, reject) => {
+  var existingUser = {};
+  axios.get(`${baseUrl}/users/${user.firebaseUid}`)
+    .then((response) => {
+      existingUser.firebaseUid = response.data.firebaseUid;
+      resolve(existingUser);
+    })
+    .catch((err) => {
+      reject(err);
+      axios.post(`${baseUrl}/users`, user);
+    });
+});
 
 const loginUser = () => {
   //sub out whatever auth method firebase provides that you want to use.
   const provider = new firebase.auth.GoogleAuthProvider();
   return firebase.auth().signInWithPopup(provider).then(cred => {
     //get token from firebase
-    let userInfo = {firebaseUid: cred.user.uid};
-  
-    cred.user.getIdToken()
-        //save the token to the session storage
-      .then(token => sessionStorage.setItem('token',token))
-      .then(() => {
-        // var userExists = checkIfNewUser(userInfo.firebaseUid);
-        // var existingUser = {};
-        // axios.get(`${baseUrl}/users/${userInfo.firebaseUid}`)
-        //   .then((response) => {
-        //     existingUser.firebaseUid = response.data.firebaseUid;
-        //     console.log('response:', response);
-        //     if(existingUser.firebaseUid) {
-        //       console.log('already got this user:', existingUser.firebaseUid); 
-        //     } else {
-        //       //axios.post(`${baseUrl}/users`, userInfo);
-        //       console.log('posting:', userInfo.firebaseUid);
-        //     }
-        //   })
+    let userInfo = { firebaseUid: cred.user.uid };
 
-        var existingUser = axios.get(`${baseUrl}/users/${userInfo.firebaseUid}`)
-          
-            console.log('response:', existingUser);
-            if(existingUser.firebaseUid) {
-              console.log('already got this user:', existingUser.firebaseUid); 
-            } else {
-              //axios.post(`${baseUrl}/users`, userInfo);
-              console.log('posting:', userInfo.firebaseUid);
-            }
-          
-        
-      }
-      )
-      // .then(() => axios.post(`${baseUrl}/users`,userInfo))
-      
-  });
+    cred.user.getIdToken()
+      //save the token to the session storage
+      .then(token => sessionStorage.setItem('token', token))
+      .then(() => {
+        checkForExistingUser(userInfo)
+      })
+  })
 };
 
 const logoutUser = () => {
@@ -81,7 +67,7 @@ const getUid = () => {
 };
 
 export default {
-    getUid, 
-    loginUser, 
-    logoutUser, 
+  getUid,
+  loginUser,
+  logoutUser,
 };
